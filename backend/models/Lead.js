d1472ab3c119223c9e9dee43229f6f2e06eb13fd
anand9208 <anand9208@outlook.com>
2025-08-31 13:26:16 +0400

    Add backend/models/Lead.js

--- backend/models/Lead.js
+++ backend/models/Lead.js
@@ -1,10 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const LeadSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  phone: { type: String },
-  createdAt: { type: Date, default: Date.now }
+const leadSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
 });
 
-module.exports = mongoose.model('Lead', LeadSchema);
+module.exports = mongoose.model("Lead", leadSchema);
